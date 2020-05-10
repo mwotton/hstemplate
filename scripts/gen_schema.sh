3e6c63e9c8e1b3e65bbbe2c87f0e08d6433a1600
Mark Wotton <mwotton@gmail.com>
2020-05-10 09:26:20 -0400

    ormolu over schema

--- scripts/gen_schema.sh
+++ scripts/gen_schema.sh
@@ -5,8 +5,4 @@ IFS=$'\n\t'
 
 uri=$(pg_tmp -w 300)
 sqitch deploy -t "${uri}"
-#psql "${uri}"
-squealgen "${uri}" Schema hstemplate
-squealgen "${uri}" Schema hstemplate > $(git rev-parse --show-toplevel)/hstemplate/src/Schema.hs
-
-echo ${uri}
+squealgen "${uri}" Schema hstemplate | ormolu > $(git rev-parse --show-toplevel)/hstemplate/src/Schema.hs
