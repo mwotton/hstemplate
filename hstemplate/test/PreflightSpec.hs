0331c8a4318fc814e986e2723acd9e8b62eccf8e
Mark Wotton <mwotton@gmail.com>
2020-05-10 20:04:47 -0400

    typesigs

--- hstemplate/test/PreflightSpec.hs
+++ hstemplate/test/PreflightSpec.hs
@@ -4,10 +4,13 @@ import System.Process (readCreateProcessWithExitCode,proc)
 import System.Exit(ExitCode(..))
 import Test.Hspec
 
+runCommand :: FilePath -> [String] -> IO (ExitCode, String, String)
 runCommand cmd args = readCreateProcessWithExitCode (proc cmd args) ""
 
+succeeds :: FilePath -> [String] -> IO ()
 succeeds cmd args = runCommand cmd args >>= flip shouldSatisfy (\(x,_,_) -> x==ExitSuccess)
 
+spec :: Spec
 spec =
   describe "preflights" $ do
     it "successfully rebuilds the schema" $ succeeds "make" ["schema"]
