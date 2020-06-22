44e4f3795daad69d99d7faadd122d94f0abbf8a2
Mark Wotton <mwotton@gmail.com>
2020-06-21 20:02:31 -0400

    test pedantic fixes

--- hstemplate/test/Spec.hs
+++ hstemplate/test/Spec.hs
@@ -1,9 +1,10 @@
-import Test.Hspec
 import qualified PreflightSpec
+import           Test.Hspec
 
 main :: IO ()
 main = hspec spec
 
+spec :: Spec
 spec = describe "hstemplate" $ do
   PreflightSpec.spec
   -- awful hack: i don't want to turn the "redundant do" hlint flag on,
