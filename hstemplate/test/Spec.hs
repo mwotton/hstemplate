a0f84fa45e25f5600187dd7fff98f0521761434f
Mark Wotton <mwotton@gmail.com>
2020-07-30 16:47:39 -0400

    use hspec-discover

--- hstemplate/test/Spec.hs
+++ hstemplate/test/Spec.hs
@@ -1,17 +1,1 @@
-{-# LANGUAGE OverloadedStrings #-}
-import qualified DBSpec
-import qualified PreflightSpec
-import           Queries
-import           Squeal.PostgreSQL
-import           Test.Hspec
-
-main :: IO ()
-main = hspec spec
-
-spec :: Spec
-spec = describe "hstemplate" $ do
-  PreflightSpec.spec
-  -- silly test to check coverage
-  it "serialises" $
-    renderSQL getAllFoosQ `shouldBe` renderSQL getAllFoosQ
-  DBSpec.spec
+{-# OPTIONS_GHC -F -pgmF hspec-discover -optF --module-name=Spec #-}
